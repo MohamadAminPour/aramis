a7b545d1edef8793f3728f3c93b322e59dba0efa
mohammad amin pour <ytornado37@gmail.com>
2025-11-03 21:59:26 +0330

    add some change

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,8 +5,8 @@ export default function Gallery() {
         <p className="text-[1.3rem] mb-1">گالری آرامیس</p>
         <p className="text-zinc-500 text-[.9rem]">آرامیس خیالتو از یه خواب راحت تخت میکنه !</p>
         <div className="w-[100%] md:w-[80%] grid grid-cols-1 lg:grid-cols-2 gap-4 p-5 mt-3">
-          <img src="../public/images/img1.png" className="w-[100%] h-[17rem] object-cover rounded-2xl" alt="" />
-          <img src="../public/images/img2.png" className="w-[100%] h-[17rem] object-cover rounded-2xl" alt="" />
+          <img src="/images/img1.png" className="w-[100%] h-[17rem] object-cover rounded-2xl" alt="" />
+          <img src="/images/img2.png" className="w-[100%] h-[17rem] object-cover rounded-2xl" alt="" />
         </div>
       </div>
     </>
